b94bab082967a3f03b4654ccdd9b3c8d79bffae3
gabrielhdt <gabrielhondet@gmail.com>
2017-11-24 15:41:06 +0100

    (+) Added essential types

--- game.mli
+++ game.mli
@@ -1,1 +1,10 @@
 (** Implements a tetris game *)
+
+(** The tetris board *)
+type board
+
+(** Tetromino, i.e. a piece of tetris, on which the player acts *)
+type tetromino
+
+(** An action executed by the player *)
+type action
