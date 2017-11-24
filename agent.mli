bddbf6f8044387c09a5ca24c29226ef7652b3837
gabrielhdt <gabrielhondet@gmail.com>
2017-11-24 18:46:03 +0100

    (-) Simplified interface. No need of play.

--- agent.mli
+++ agent.mli
@@ -2,7 +2,4 @@
 
 (** Builds a Q matrix used by agent to determine actions *)
 val train : float array array -> float -> float -> (int -> float) -> int ->
-  float array array
-
-val play : float array array -> float -> Game.board -> Game.tetromino ->
-  Game.action array -> Game.action
+  Game.board
