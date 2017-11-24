fb2fa02a3adbd15f0fb33f61097aadd72cf60b6a
gabrielhdt <gabrielhondet@gmail.com>
2017-11-24 15:38:34 +0100

    (+) Created interface agent

--- agent.mli
+++ agent.mli
@@ -1,1 +1,8 @@
 (** Agent module, builds the Q matrix used by policy *)
+
+(** Builds a Q matrix used by agent to determine actions *)
+val train : float array array -> float -> float -> (int -> float) -> int ->
+  float array array
+
+val play : float array array -> float -> Game.board -> Game.tetromino ->
+  Game.action array -> Game.action
