d0dd634cd0f5ccb5be34b72afedaf87a0a1d20e9
gabrielhdt <gabrielhondet@gmail.com>
2017-11-25 13:08:19 +0100

    (+) Convenience function (board manipulation)

--- game.mli
+++ game.mli
@@ -11,6 +11,13 @@ module Board :
 
     (** Gives the height of the given board, i.e. number of stages stacked *)
     val height : t -> int
+
+    (** Creates a representation of the board *)
+    val to_arr : t -> int -> int -> bool array array
+    (** {e to_arr board low up} outputs a matrix representing the board
+        {e board} from line {e low} to line {e up}
+        @return boolean matrix with true if block occupied by a tetromino, else
+                false *)
   end
 
 (** Manipulates tetrominos in the board *)
