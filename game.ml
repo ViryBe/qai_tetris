69b5b31a798e3ea7270bf360f02d506d58d7a358
Claire Cousin <claire.cousin@alumni.enac.fr>
2017-11-26 15:16:44 +0000

    (+) function play Add function play and the other functions needed to make it work

--- game.ml
+++ game.ml
@@ -15,6 +15,9 @@ module Board = struct
      stacked_height = 0
     }
 
+  let update_board board height =
+    {board = board; stacked_height = height}
+
   (** Gives the height of the given board, i.e. number of stages stacked *)
   let height b = b.stacked_height
 
@@ -23,6 +26,9 @@ module Board = struct
   (** [to_arr l u b] outputs a matrix representing the board
       [b] from line [l] to line [u] (included) where each element is
       one if the square is occupied by a tetromino else zero *)
+
+  (** Get the board *)
+  let get_board b = b.board
 end
 
 module Tetromino = struct
@@ -75,8 +81,10 @@ module Action = struct
   (** 2D to 1D Array - index match *)
   let get_index i j = 2*i + j
 
+  let get_rotation action = action.rot
+
   (** Give the correct index for 1D Array after a rotation *)
-  let get_rotation rotation i j =
+  let make_rotation rotation i j =
     let n = get_index i j in
     match rotation with
     | North -> [|0;1;2;3|].(n)
@@ -88,12 +96,45 @@ end
 
 let tetromino_per_game = 10000
 
-let rotation_90 i j =
-  let height, width = 2,2 in
-  2*(height + 1 - j ) + i
+let collide table x y tetromino rotation =
+  let n = ref(0) in
+  for i=0 to 1 do
+    for j=0 to 1 do
+      n := (Tetromino.to_arr tetromino).(Action.make_rotation rotation i j) + (Board.get_board table).(x+i).(y+j)
+    done;
+  done;
+  !n > 4
 
-  (*
+let update_height board x y =
+  if board.(x).(y) = 0 && board.(x).(y+1) = 0 then x else x-1
+
+let place_tetromino table tetromino rotation x y =
+  let board = Board.get_board table in
+  for i=0 to 1 do
+    for j=0 to 1 do
+      board.(x+i).(j+y) <- board.(x+i).(y+j) + (Tetromino.to_arr  tetromino).(Action.make_rotation rotation i j)
+    done;
+  done;
+  Board.update_board board (max (update_height board x y) (Board.height table))
+
+let is_full board x =
+  let n = ref(0) in
+  for i=0 to 5 do
+    if board.(x).(i) = 1 then n := !n + 1;
+  done;
+  !n = 6
+
 let play board tetromino action =
-  let x = Action.int_translation action in
-  x
-  *)
+  let x = ref(Board.height board + 1) in (* +1 to add the new tetromino *)
+  let y = Action.int_translation action in
+  while not (collide board !x y tetromino (Action.get_rotation action)) &&  !x > 0 do
+    x := !x - 1
+  done;
+  let t = place_tetromino board tetromino (Action.get_rotation action) !x y in
+  for i=0 to 1 do
+    let table = Board.get_board t in
+    let line = !x - i in
+    if is_full table line then
+      Array.blit table (line+1) table line ((Board.height t)-line-1)
+  done;
+  t
