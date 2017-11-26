088dddabb511eb0be74e628473faed6e49331344
gabrielhdt <gabrielhondet@gmail.com>
2017-11-26 10:24:26 +0100

    (+) Rewritten doc, added card of actions

--- game.mli
+++ game.mli
@@ -1,45 +1,52 @@
 (** Implements a tetris game *)
 
 (** Module using the overall board *)
-module Board :
-  sig
-    (** The tetris board *)
-    type t
+module Board : sig
+  (** The tetris board *)
+  type t
 
-    (** Creates an empty board *)
-    val create : unit -> t
+  (** Creates an empty board *)
+  val create : unit -> t
 
-    (** Gives the height of the given board, i.e. number of stages stacked *)
-    val height : t -> int
+  (** Gives the height of the given board, i.e. number of stages stacked *)
+  val height : t -> int
 
-    (** Creates a representation of the board *)
-    val to_arr : int -> int -> t -> int array array
-    (** {e to_arr low up board} outputs a matrix representing the board
-        {e board} from line {e low} to line {e up}
-        @return boolean matrix with true if block occupied by a tetromino, else
-                false *)
-  end
+  (** Creates a representation of the board *)
+  val to_arr : int -> int -> t -> int array array
+  (** [to_arr l u b] outputs a matrix representing the board
+      [b] from line [l] to line [u] (included) where each element is
+      one if the square is occupied by a tetromino else zero *)
+end
 
 (** Manipulates tetrominos in the board *)
-module Tetromino :
-  sig
-    (** Tetromino type *)
-    type t
+module Tetromino : sig
+  (** Tetromino type *)
+  type t
 
-    (** Generates a random tetromino *)
-    val create_ran : unit -> t
+  (** Generates a random tetromino *)
+  val create_ran : unit -> t
 
-    (** Outputs an array representation of a tetromino *)
-    val to_arr : t -> int array
-  end
+  (** Outputs an array representation of a tetromino *)
+  val to_arr : t -> int array
+end
 
 (** An action executed by the player *)
 module Action : sig
+
+  (** The type of an action *)
   type t
 
+  (** Number of available actions *)
+  val card : int
+
+  (** Extracts the rotation component of an action *)
   val rotation : t -> int
+
+  (** Extracts the translation component of an action *)
   val translation : t -> int
 end
 
 (** Plays a turn and gives updated board *)
 val play : Board.t -> Tetromino.t -> Action.t -> Board.t
+(** [play b t a] plays the action [a] on tetromino [t] in board [b] and returns
+    a new board with the consequences of the turn *)
