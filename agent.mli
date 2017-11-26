392b81d3231e368930ecda6c1e8308fa119bd256
gabrielhdt <gabrielhondet@gmail.com>
2017-11-26 10:12:30 +0100

    (+) Such a nice doc.

--- agent.mli
+++ agent.mli
@@ -2,4 +2,13 @@
 
 (** Builds a Q matrix used by agent to determine actions *)
 val train : float array array -> float -> float -> (int -> float) -> int ->
-  Game.Board.t
+  Game.Action.t array -> int -> unit
+(** [train q e g ak ng A nt] trains the matrix [q] with [e] the frequency of
+    random choice,
+    [g] the sight length of the agent, i.e. weight given to future turns to
+    make an action,
+    [ak : int -> float] the learning rate,
+    [ng] the number of games played to train,
+    [A] the set of actions the agent is able to perform and
+    [nt] number of tetrominos played in each game
+*)
