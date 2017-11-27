593ef1053a1a6b1ed4133cf45fecddc09fc59908
gabrielhdt <gabrielhondet@gmail.com>
2017-11-27 08:51:04 +0100

    (~) Corrected interfaces

--- agent.mli
+++ agent.mli
@@ -2,18 +2,17 @@
 
 (** Builds a Q matrix used by agent to determine actions *)
 val train : float array array -> float -> float -> (int -> float) -> int ->
-  Game.Action.t array -> int -> unit
-(** [train q e g ak ng A nt] trains the matrix [q] with [e] the frequency of
+  int -> unit
+(** [train q e g ak ng nt] trains the matrix [q] with [e] the frequency of
     random choice,
     [g] the sight length of the agent, i.e. weight given to future turns to
     make an action,
     [ak : int -> float] the learning rate,
     [ng] the number of games played to train,
-    [A] the set of actions the agent is able to perform and
     [nt] number of tetrominos played in each game
 *)
 
 (** Plays a game *)
-val play : float array array -> int -> int
-(** [play q nt] gives the score of the game composed of [nt] tetrominos played
+val play : float array array -> int -> Game.Board.t
+(** [play q nt] gives the final board of the game composed of [nt] tetrominos
     played with matrix [q] *)
