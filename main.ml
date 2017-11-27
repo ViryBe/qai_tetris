83fc5fa1dcba0fa90dbac41816c8f1626bfa46b9
gabrielhdt <gabrielhondet@gmail.com>
2017-11-27 09:02:10 +0100

    (+) Functions launched

--- main.ml
+++ main.ml
@@ -4,14 +4,18 @@
 type glob_params = {
   epsilon : float ref;
   gamma : float ref;
+  alphap : float ref;
   ngames : int ref;
   demo : bool ref;
   qpath : string ref;
 }
 
 (** Init command line params *)
-let cl_params = {epsilon = ref 0. ; gamma = ref 0. ; ngames = ref 0 ;
-                 demo = ref false ; qpath = ref ""}
+let cl_params = {epsilon = ref 0. ; gamma = ref 0. ; alphap = ref 0. ;
+                 ngames = ref 0 ; demo = ref false ; qpath = ref ""}
+
+(** Number of states *)
+let state_card = 20480
 
 
 (** Checks adequation of input float parameter
@@ -30,13 +34,14 @@ let check_qpath given_qpath =
 
 (** Speclist for argument parsing *)
 let speclist = [
-  ("-demo", Arg.Set cl_params.demo, ": set demo mode");
-  ("-q", Arg.String check_qpath, ": set path of Q matrix file");
-  ("-n", Arg.Set_int cl_params.ngames, ": set number of games played");
+  ("-demo", Arg.Set cl_params.demo, "demo mode");
+  ("-q", Arg.String check_qpath, "path of Q matrix file");
+  ("-n", Arg.Set_int cl_params.ngames, "number of games played");
   ("-epsilon", Arg.Float (float_check cl_params.epsilon),
-   ": set frequency of random action, in [0, 1]");
+   "frequency of random action, in [0, 1]");
   ("-gamma", Arg.Float (float_check cl_params.gamma),
-   ": set sight length of the policy, in [0, 1]");
+   "sight length of the policy, in [0, 1]");
+  ("-alpha", Arg.Set_float cl_params.alphap, "parameter of the learning rate");
 ]
 
 let () =
@@ -49,5 +54,18 @@ let () =
     usage;
   (* Init logconf *)
   Bolt.Logger.log "main" Bolt.Level.INFO "Qai tetris started" ;
-  Printf.printf "%f %f %d %b\n" !(cl_params.epsilon) !(cl_params.gamma)
-    !(cl_params.ngames) !(cl_params.demo)
+  Printf.printf "Parameters: eps=%f:gam=%f:ngames=%d\n" !(cl_params.epsilon)
+    !(cl_params.gamma) !(cl_params.ngames) ;
+  (* Launching program *)
+  if !(cl_params.demo)
+  then if !(cl_params.qpath) = ""
+    then raise (Arg.Bad "Demo mode requires qpath")
+    else let qmat = Aio.load_mat !(cl_params.qpath) in
+      ignore (Agent.play qmat Game.tetromino_per_game)
+  else if !(cl_params.qpath) = ""
+  then let qinit = Array.make_matrix state_card
+           (Array.length Game.Action.set) 0. in
+    Agent.train qinit !(cl_params.epsilon) !(cl_params.gamma)
+      (fun k -> 1. /. (1. +. !(cl_params.alphap) *. float k))
+      !(cl_params.ngames)
+      Game.tetromino_per_game
