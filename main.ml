c8d0af7d0e4d1b4d7c9a59efeea1d599522dfc65
gabrielhdt <gabrielhondet@gmail.com>
2017-11-30 16:52:10 +0100

    (~) Matrix saved in main, and not in agent.

--- main.ml
+++ main.ml
@@ -3,10 +3,18 @@
 (** Number of states *)
 let state_card = 20480 
 
+(** Used for optional arguments:
+    @param x the optional argument
+    @param f the function calling x *)
+let may ~f x =
+  match x with
+  | None -> ()
+  | Some x -> ignore (f x)
+
 let () =
   Random.self_init () ;
   (* Load command line parameters *)
-  let cl_params =
+  let clargs =
     try
       Aio.Clargs.parse Sys.argv
     with
@@ -16,19 +24,21 @@ let () =
   (* Init logconf *)
   Bolt.Logger.log "main" Bolt.Level.INFO "Qai tetris started" ;
   (* Launching program *)
-  if cl_params.demo (* Demo mode *)
-      then match cl_params.qload with
+  if clargs.demo (* Demo mode *)
+      then match clargs.qload with
       | Some qpath -> let qmat = Aio.Qio.load qpath in
-          ignore (Agent.play qmat cl_params.ntetr)
+          ignore (Agent.play qmat clargs.ntetr)
       | None -> raise (Arg.Bad "demo mode requires -qload")
   else
     (* Set Q matrix (load or create *)
     let qinit =
-      match cl_params.qload with
+      match clargs.qload with
       | None -> Array.make_matrix state_card (Array.length Game.Action.set) 0.
       | Some str -> Aio.Qio.load str
     in
     (* Start training *)
-    Agent.train ?qpath:cl_params.qsave qinit cl_params.epsilon cl_params.gamma
-      (fun k -> 1. /. (1. +. cl_params.alphap *. float k))
-      cl_params.ngames cl_params.ntetr
+    Agent.train qinit clargs.epsilon clargs.gamma
+      (fun k -> 1. /. (1. +. clargs.alphap *. float k))
+      clargs.ngames clargs.ntetr ;
+    (* Save matrix if qsave path is specified *)
+    may (Aio.Qio.save qinit) clargs.qsave
