b854816a33d065c08fd577c84dc471a3c6f18420
gabrielhdt <gabrielhondet@gmail.com>
2017-12-08 09:16:00 +0100

    (~) Relocated graphics code, some pre features

--- main.ml
+++ main.ml
@@ -12,10 +12,6 @@ let may ~f x =
   | Some x -> ignore (f x)
 
 let () =
-  (* init graphics window *)
-  let size = (" "^(string_of_int (Display.total_width))
-              ^"x"^(string_of_int (Display.total_height))) in
-  Graphics.open_graph (size);
 
   Random.self_init () ;
   (* Load command line parameters *)
@@ -34,14 +30,22 @@ let () =
   (* Launching program *)
   if demo (* Demo mode *)
       then match qload with
-      | Some qpath -> let qmat = Aio.Qio.load qpath in
-          ignore (Agent.play qmat ntetr)
+      | Some qpath ->
+          begin
+            (* init graphics window *)
+            let size = (" " ^ (string_of_int (Display.total_width)) ^
+                        "x" ^ (string_of_int (Display.total_height))) in
+            Graphics.open_graph (size) ;
+            let qmat = Aio.Qio.load qpath in
+            let _ = Agent.play qmat ntetr in
+            Graphics.close_graph ()
+          end
       | None -> raise (Arg.Bad "demo mode requires -qload")
   else
     (* Set Q matrix (load or create *)
     let qmat =
       match qload with
-      | None -> Array.make_matrix state_card (Array.length Game.Action.set) 0.
+      | None -> Array.make_matrix state_card Game.Action.card 0.
       | Some str -> Aio.Qio.load str
     and alpha k = 1. /. (1. +. alphap *. float k)
     in
@@ -51,6 +55,3 @@ let () =
     Agent.train qmat eps gam alpha ngames ntetr ;
     (* Save matrix if qsave path is specified *)
     may (Aio.Qio.save qmat) qsave;
-
-  (* close window *)
-  Graphics.close_graph ();;
