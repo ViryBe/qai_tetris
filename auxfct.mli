08dece2f78926c3b5e5f92b2a16abfd017c10ea5
ViryBe <benoit.viry@laposte.net>
2017-12-13 16:01:11 +0100

    (+) module auxfxt

---
+++ auxfct.mli
@@ -0,0 +1,5 @@
+val flarray_max : 'a array -> 'a
+val argmax_r : float array -> int
+val arr2dig : int array -> int
+val get_board_top : Game.Board.t -> int array array
+val arr_find : 'a array -> 'a -> int
