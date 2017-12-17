e55ddfe61b33cd9d697938ebe387b00e991b9420
ViryBe <benoit.viry@laposte.net>
2017-12-17 18:02:46 +0100

    (+) iter2

--- auxfct.mli
+++ auxfct.mli
@@ -10,5 +10,9 @@ val fold_left2 : ('a -> 'b -> 'c -> 'a) -> 'a -> 'b array -> 'c array -> 'a
 (* in the stdlib only since ocaml 4.03.0 *)
 val map2 : ('a -> 'b -> 'c) -> 'a array -> 'b array -> 'c array
 
+(** iter2 on 2 arrays *)
+(* in the stdlib only since ocaml 4.03.0 *)
+val iter2 : ('a -> 'b -> unit) -> 'a array -> 'b array -> unit
+
 (** dot product *)
 val dot : float array -> float array -> float
